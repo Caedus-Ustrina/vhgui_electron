facf2ec67759d42f87c452c803fd11e55c545d4d
Damion <dcreiglow@ewu.edu>
2024-03-28 10:58:53 -0600

    Improved descriptions for talents and abilities. Talents are colored and formatted.

--- src/main/InterfaceOrganizer.js
+++ src/main/InterfaceOrganizer.js
@@ -5,39 +5,44 @@ import skillDescriptions from "/skill_descriptions.json";
 import classNames from "classnames";
 
 export const OrganizedInterface = () => {
-    const [description, setDescription] = useState("");
+    const [description, setDescription] = useState([]);
     const [displayDescription , setDisplayDescription] = useState(false);
 
     function handleAbilityDescription(e) {
-        var fullText = "";
-        abilities_descriptions.data[e].description.forEach(element => {
-                    fullText = fullText + element.text
-                }
-            );
-        setDescription(fullText);
+        setDescription(abilities_descriptions.data[e].description);
         setDisplayDescription(true);
     }
     
     function handleTalentDescription(e) {
-        var fullText = "";
-        var description = skillDescriptions.descriptions[e]
-        description.forEach(element => {
-                    fullText = fullText + element.text
-                }
-            );
-        setDescription(fullText);
+        setDescription(skillDescriptions.descriptions[e]);
         setDisplayDescription(true);
+    }
+
+    function incrementId(){
+        setDescriptionUID(descriptionUID + 1);
     }
 
     return <div className={classNames("OrganizedInterfaceFull", {"OrganizedInterfacePartial" : displayDescription})}>
         <BuildInterface handleAbilityDescription={handleAbilityDescription} 
         handleTalentDescription = {handleTalentDescription}/>
-        {displayDescription ? <DescriptionsBox description = {description}/> : <></>}
+        {displayDescription ? <DescriptionsBox description = {description}/> : null}
     </div>
 }
 
 const DescriptionsBox = (props) => {
     return <div className="DescriptionBox">
-        {props.description}
+        {props.description.map((textSection, index) => {
+            let color = textSection.color || "black";
+            return <React.Fragment key={textSection.text + index}>
+            <span style={{color: color}}>
+                {textSection.text.split(/\n/).length > 1 ? 
+                textSection.text.split(/\n/).map((line, index) => {
+                    return line !== "" && line !== " "? <React.Fragment key={line + index}> {line} </React.Fragment> :
+                    <br key={index}/>
+                }):
+                textSection.text}
+            </span>
+            </React.Fragment>
+        })}
     </div>
 }
